54417d4482eb10dab244cc3efcf418a9c53ef245
Daniel Alksnis <daniel.alksnis@glencore.ca>
2025-10-14 15:33:47 -0400

    Parrot config updates

--- init.lua
+++ init.lua
@@ -902,6 +902,16 @@ require('lazy').setup({
     end,
   },
 
+  { -- Gruvbox
+    'sainnhe/gruvbox-material',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      vim.cmd.set 'termguicolors'
+      vim.cmd.set 'background=light'
+      vim.cmd.colorscheme 'gruvbox-material'
+    end,
+  },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -920,7 +930,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
+      -- vim.cmd.colorscheme 'tokyonight-moon'
     end,
   },
 
